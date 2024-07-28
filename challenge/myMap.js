27f33c6ed91db0b85e9f23cd878e02d7cc8de3c1
ZoveRKI <shiyu5645@gmail.com>
2024-07-29 02:25:32 +0900

    myMap function: version3

--- challenge/myMap.js
+++ challenge/myMap.js
@@ -1,17 +1,21 @@
 function myMap(list, fun, defult) {
     temp = []
+    temp2 = []
     list.reduce(function (a, b) {
         temp.push(fun(a))
+        temp2.push(defult[a])
         return b
     })
-    return temp
+    var result = (defult) ? temp2 : temp;
+    return result
 }
 
-
 a = [1, 2, 3, 4, 5]
+aka = ['a', 'b', 'c', 'd', 'e', 'f']
 d = myMap(a, function (n, p, q) {
     p = a.indexOf(n)
     q = a
     return n * 2
-})
+}, aka)
+
 console.log(d)
