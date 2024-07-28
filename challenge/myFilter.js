f66f463f288af4812182765638f36715a9beb92f
ZoveRKI <shiyu5645@gmail.com>
2024-07-29 03:00:38 +0900

    myFilter function: version2

--- challenge/myFilter.js
+++ challenge/myFilter.js
@@ -1,7 +1,7 @@
 function myFilter(list, fun, defult) {
     temp = []
     list.reduce(function (a, b) {
-        if (fun(a)) {
+        if (fun(a) && a > defult.MAX) {
             temp.push(a)
         }
         return b
@@ -9,8 +9,13 @@ function myFilter(list, fun, defult) {
     return temp
 }
 
-a = [1, 2, 3, 4, 5]
+var a = [1, 2, 3, 4, 5]
+var obj = { MAX: 3 }
 d = myFilter(a, function (n, p, q) {
+    p = a.indexOf(n)
+    q = a
+    // if (n > this.MAX) {
     return n % 2 === 0
-})
+    // }
+}, obj)
 console.log(d)
